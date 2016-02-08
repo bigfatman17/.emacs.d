22b4932fa7c3ca397a678f873f951f061f22c7a4
armv7 <bigfatman17@gmail.com>
2016-02-08 16:18:30 -0500

    Removed.

--- elpa/auto-complete-clang-20140409.52/auto-complete-clang-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; auto-complete-clang-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("auto-complete-clang-pkg.el" "auto-complete-clang.el")
-;;;;;;  (22197 6983 460120 587000))
-
-;;;***
-
-(provide 'auto-complete-clang-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; auto-complete-clang-autoloads.el ends here
